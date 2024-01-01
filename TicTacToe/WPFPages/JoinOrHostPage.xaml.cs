af28ec6566f632e6d4896b7e52cad1a2cc31b064
ValentinStoqnov <sqdodo@gmail.com>
2024-01-02 01:44:05 +0200

    Implementing JoinOrHostPage

---
+++ TicTacToe/WPFPages/JoinOrHostPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+
+
+namespace TicTacToe.WPFPages
+{
+    /// <summary>
+    /// Interaction logic for JoinOrHostPage.xaml
+    /// </summary>
+    public partial class JoinOrHostPage : Page
+    {
+        public JoinOrHostPage()
+        {
+            InitializeComponent();
+        }
+
+        private void JoinGameBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void HostGameBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
