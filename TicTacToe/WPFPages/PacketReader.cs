bbe3527250c09875493afe3fb8c87081f3910a1f
ValentinStoqnov <sqdodo@gmail.com>
2024-01-05 09:37:09 +0200

    Created PacketReader

---
+++ TicTacToe/WPFPages/PacketReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+
+namespace TicTacToe.WPFPages
+{
+    public class PacketReader
+    {
+        int OperationFlag;
+        int MessageLength;
+        byte[] MessageDataBytes;
+
+        public PacketReader(byte[] BytesToRead)
+        {
+            MemoryStream ms = new MemoryStream(BytesToRead);
+            OperationFlag = ms.ReadByte();
+            byte[] messageLengthBuffer = new byte[4];
+            ms.Read(messageLengthBuffer, 0, 4);
+            MessageLength = BitConverter.ToInt32(messageLengthBuffer, 0);
+            MessageDataBytes = new byte[MessageLength];
+            ms.Read(MessageDataBytes, 0, MessageLength);
+            
+            
+        }
+        public OperationFlags GetOperationFlag()
+        {
+            switch (OperationFlag)
+            {
+                case 1:
+                    return OperationFlags.MessageString;
+                case 2:
+                    return OperationFlags.Actions;
+                default:
+                    return OperationFlags.Null;
+
+            }
+        }
+        public string GetMessageString()
+        {
+            Debug.WriteLine(Encoding.UTF8.GetString(MessageDataBytes, 0, MessageLength));
+            return Encoding.UTF8.GetString(MessageDataBytes, 0, MessageLength);
+        }
+    }
+}
