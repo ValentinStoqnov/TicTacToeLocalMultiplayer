1e6d23b8b6e00dcd84a12215db1b4235ff226ffc
ValentinStoqnov <sqdodo@gmail.com>
2024-01-05 09:37:32 +0200

    Implemented PacketReader in ClientLogic

--- TicTacToe/ClientLogic.cs
+++ TicTacToe/ClientLogic.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TicTacToe.WPFPages;
+using System.Collections.ObjectModel;
 
 namespace TicTacToe
 {
@@ -13,18 +13,51 @@ namespace TicTacToe
     {
         public ClientLogic()
         {
-            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress hostIp = host.AddressList[1];
-            IPEndPoint hostEndPoint = new IPEndPoint(hostIp, 12500);
 
-            Socket hostSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            hostSocket.Connect(hostEndPoint);
-            if (hostSocket.Connected)
+        }
+
+        private static List<IPEndPoint> GetActiveTcpServerListeners()
+        {
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] endPoints = properties.GetActiveTcpListeners();
+            List<IPEndPoint> listeningServersList = new List<IPEndPoint>();
+
+            foreach (IPEndPoint e in endPoints)
             {
-                Debug.WriteLine("WE HAVE CONNECTION");
+                if (e.Port == 12500)
+                    listeningServersList.Add(e);
             }
-
+            return listeningServersList;
+        }
+        public async static Task<ObservableCollection<string>> GetAllServerNames()
+        {
+            ObservableCollection<string> serverNames = new ObservableCollection<string>();
+            List<IPEndPoint> serverList = GetActiveTcpServerListeners();
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            foreach (IPEndPoint e in serverList)
+            {
+                try
+                {
+                     clientSocket.Connect(e); ////////////////////////////// needs to be async
+                }
+                catch
+                {
+                    Debug.WriteLine($"{e.Address} ServerUnreachable");
+                }
+                finally
+                {
+                    if (clientSocket.Connected == true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        clientSocket.Receive(buffer, SocketFlags.None); ///////////////////////////////////needs to be async
+                        PacketReader packetReader = new PacketReader(buffer);
+                        serverNames.Add(packetReader.GetMessageString());
+                    }
+                }
+            }
+            return serverNames;
         }
     }
 }
+
