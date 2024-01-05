5334b40b53dbdc99c16a0605854a53d0480a04e5
ValentinStoqnov <sqdodo@gmail.com>
2024-01-05 09:36:55 +0200

    Created PacketSender

---
+++ TicTacToe/PacketSender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class PacketSender
+    {
+        MemoryStream memoryStream;
+        public PacketSender()
+        {
+            memoryStream = new MemoryStream();
+        }
+        public void WriteOperationFlag(OperationFlags operationFlags)
+        {
+            memoryStream.WriteByte((byte)operationFlags);
+        }
+        public void WriteString(string msg)
+        {
+            memoryStream.Write(BitConverter.GetBytes(msg.Length));
+            memoryStream.Write(Encoding.UTF8.GetBytes(msg));
+        }
+        public byte[] GetPacketBytes()
+        {
+            return memoryStream.ToArray();
+        }
+    }
+}
