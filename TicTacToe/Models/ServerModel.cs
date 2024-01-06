eae2ce0fcf6c59cf2bae04ce7e829b2f6998fb68
ValentinStoqnov <sqdodo@gmail.com>
2024-01-07 00:17:08 +0200

    Created ServerModel class

---
+++ TicTacToe/Models/ServerModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class ServerModel
+    {
+		private string name = "";
+        private string ipAddress = "";
+
+		public ServerModel(string ipAddress, string serverName)
+		{
+			ServerIp = ipAddress;
+			ServerName = serverName;
+		}
+
+		public string ServerName
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		
+		public string ServerIp
+		{
+			get { return ipAddress; }
+			set { ipAddress = value; }
+		}
+
+
+	}
+}
