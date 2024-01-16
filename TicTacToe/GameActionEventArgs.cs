3278f19d43317c29637eee06d159e706d46a5cef
ValentinStoqnov <sqdodo@gmail.com>
2024-01-16 20:57:46 +0200

    Added PlayerMark property to the GameAction event arguments.

--- TicTacToe/GameActionEventArgs.cs
+++ TicTacToe/GameActionEventArgs.cs
@@ -9,10 +9,12 @@ namespace TicTacToe
     public class GameActionEventArgs : EventArgs
     {
         public GameActions receivedGameAction { get; set; }
+        public PlayerMark receivedPlayerMark { get; set; }
 
-        public GameActionEventArgs(GameActions gameActions)
+        public GameActionEventArgs(GameActions gameActions, PlayerMark playerMark)
         {
             receivedGameAction = gameActions;
+            receivedPlayerMark = playerMark;
         }
     }
 }
